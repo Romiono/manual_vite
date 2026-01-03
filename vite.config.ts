07f23e35dcf4a0f8f4f26355daea183aead9a014
romashka <roma.serebrennikov.00@mail.ru>
2026-01-03 16:29:16 +0300

    Добавлен readme и структура fsd

--- vite.config.ts
+++ vite.config.ts
@@ -4,41 +4,41 @@ import { ConfigEnv, defineConfig, UserConfig } from 'vite';
 import svgr from 'vite-plugin-svgr';
 
 type ViteConfig = Omit<ConfigEnv, 'mode'> & {
-    mode: 'development' | 'production';
+	mode: 'development' | 'production';
 };
 
 export default defineConfig(({ mode }: ViteConfig): UserConfig => {
-    return {
-        root: path.resolve(__dirname, 'src'),
-        base: './',
-        publicDir: path.resolve(__dirname, 'public'),
+	return {
+		root: path.resolve(__dirname, 'src'),
+		base: './',
+		publicDir: path.resolve(__dirname, 'public'),
 
-        resolve: {
-            alias: {
-                '@': path.resolve(__dirname, './src'),
-            },
-        },
+		resolve: {
+			alias: {
+				'@': path.resolve(__dirname, './src'),
+			},
+		},
 
-        build: {
-            outDir: mode === 'production' ? path.resolve(__dirname, 'dist') : path.resolve(__dirname, 'dist-dev'),
-            emptyOutDir: true,
-            sourcemap: true,
-        },
+		build: {
+			outDir: mode === 'production' ? path.resolve(__dirname, 'dist') : path.resolve(__dirname, 'dist-dev'),
+			emptyOutDir: true,
+			sourcemap: true,
+		},
 
-        css: {
-            modules: {
-                generateScopedName: '--mv-[local]-[hash:base64:5]',
-                localsConvention: 'camelCase',
-            },
-        },
-        plugins: [
-            svgr({
-                svgrOptions: {
-                    icon: true,
-                    ref: true,
-                },
-            }),
-            react(),
-        ],
-    };
+		css: {
+			modules: {
+				generateScopedName: '--dt-[local]-[hash:base64:5]',
+				localsConvention: 'camelCase',
+			},
+		},
+		plugins: [
+			svgr({
+				svgrOptions: {
+					icon: true,
+					ref: true,
+				},
+			}),
+			react(),
+		],
+	};
 });
